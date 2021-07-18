3b8fd32c2f83d75722556a715f17ca7c0cdbf895
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-07-18 13:01:03 +0700

    clean up project to begin Business card app

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -9,28 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(UIColor.systemTeal).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            VStack {
-                Text("I Am Rich")
-                    .font(.system(size: 40))
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.blue)
-                    .padding()
-                ColorPicker("Title", selection: .constant(.red))
-                Image("diamond")
-                    .resizable().aspectRatio(contentMode: .fit)
-                    .frame(width: 200.0, height: 200.0, alignment: .center)
-                    .accentColor(.red)
-            }
-            
-        }
+        Text("I Am Rich")
         
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()//.previewDevice(PreviewDevice("Apple Watch Series 6 - 40mm"))
+        ContentView()
     }
 }
