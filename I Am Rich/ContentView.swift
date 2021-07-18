cad71cd713dac94b72987eb8669fe106f3c83c99
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-07-18 15:33:40 +0700

    finish ui layout

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("I Am Rich")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52) //0-1: none to full bar
+                .edgesIgnoringSafeArea(.all)
+//                .ignoresSafeArea(.container, edges: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("ann")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Ann Siroratt Suntronsuk")
+                    .font(Font.custom("Lobster-Regular", size: 30))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("software consultant") //job title
+                    .foregroundColor(.white)
+                    .font(Font.system(size: 25))
+                Divider()
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.white)
+                    .frame(height: 40)
+//                    .foregroundColor(.white)
+                    .overlay(HStack {
+                        Image(systemName: "phone.fill")
+                            .foregroundColor(.green)
+                        Text("+ 6694 555 5555")
+                    }).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                
+            }
+        }
         
     }
 }
