780f63d688107a6aea5ef172aae6020b39397496
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-07-18 12:48:22 +0700

    completed proj with iAmRich

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(UIColor.systemTeal).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                    .padding()
+                ColorPicker("Title", selection: .constant(.red))
+                Image("diamond")
+                    .resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0, alignment: .center)
+                    .accentColor(.red)
+            }
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView()//.previewDevice(PreviewDevice("Apple Watch Series 6 - 40mm"))
     }
 }
